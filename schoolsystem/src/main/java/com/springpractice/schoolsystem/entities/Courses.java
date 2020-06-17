277be939764be5dd4dc459d0c803ef3571d962ba
Abely <abelandk@gmail.com>
2020-06-17 22:53:24 +0200

    join table updated for courses and students entity

--- schoolsystem/src/main/java/com/springpractice/schoolsystem/entities/Courses.java
+++ schoolsystem/src/main/java/com/springpractice/schoolsystem/entities/Courses.java
@@ -1,9 +1,14 @@
 package com.springpractice.schoolsystem.entities;
 
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -17,6 +22,14 @@ public class Courses {
 	private String code;
 	private String name;
 	
+	@ManyToMany
+	@JoinTable(
+			 name = "student_course", //  name is the name of the join table in the database
+			 joinColumns = @JoinColumn(name = "course_id"), // the name of the column in the database
+			 inverseJoinColumns = @JoinColumn(name = "student_id") // the joined column name in the database
+			)
+	private List<Students> students;
+	
 	public Courses() {
 
 	}
@@ -29,20 +42,33 @@ public class Courses {
 	public int getId() {
 		return id;
 	}
+	
 	public void setId(int id) {
 		this.id = id;
 	}
+	
 	public String getCode() {
 		return code;
 	}
+	
 	public void setCode(String code) {
 		this.code = code;
 	}
+	
 	public String getName() {
 		return name;
 	}
+	
 	public void setName(String name) {
 		this.name = name;
+	}
+	
+	public List<Students> getStudents() {
+		return students;
+	}
+
+	public void setStudents(List<Students> students) {
+		this.students = students;
 	}
 
 }
--- schoolsystem/src/main/java/com/springpractice/schoolsystem/entities/Students.java
+++ schoolsystem/src/main/java/com/springpractice/schoolsystem/entities/Students.java
@@ -1,10 +1,15 @@
 package com.springpractice.schoolsystem.entities;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -20,6 +25,14 @@ public class Students {
 	
 	@Column(name="last_name")
 	private String lastName;
+	
+	@ManyToMany
+	@JoinTable(
+			name = "student_course",  //  name is the name of the join table in the database
+			joinColumns = @JoinColumn(name = "student_id"), // the name of the column in the database
+			inverseJoinColumns = @JoinColumn(name = "course_id") // the joined column name in the database
+			)
+	private List<Courses> courses;
 	
 	public Students() {
 		
@@ -53,6 +66,14 @@ public class Students {
 		this.firstName = firstName;
 	}
 
+	public List<Courses> getCourses() {
+		return courses;
+	}
+
+	public void setCourses(List<Courses> courses) {
+		this.courses = courses;
+	}
+	
 	@Override
 	public String toString() {
 		return "Students [id=" + id + ", name=" + firstName + "]";
